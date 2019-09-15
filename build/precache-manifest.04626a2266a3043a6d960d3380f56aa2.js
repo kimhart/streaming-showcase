f2848bd55af2d8d37d3c84679dbb169f3b056175
kimhart <kim@jwplayer.com>
2019-09-15 17:32:12 -0400

    build folder unignored

---
+++ build/precache-manifest.04626a2266a3043a6d960d3380f56aa2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b1ac9a4b37961043834",
+    "url": "/static/js/main.0b1ac9a4.chunk.js"
+  },
+  {
+    "revision": "3375556207372119a681",
+    "url": "/static/js/1.33755562.chunk.js"
+  },
+  {
+    "revision": "0b1ac9a4b37961043834",
+    "url": "/static/css/main.a3e7ef99.chunk.css"
+  },
+  {
+    "revision": "4f83409e39fbcce98cd5d6379d5e6088",
+    "url": "/index.html"
+  }
+];
